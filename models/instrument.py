deecb08a7c6d7f2371dab47d46f330ae79dbe901
kcirnez1211 <shekharshashank1211@gmail.com>
2024-03-27 19:26:18 +0530

    Bot Created

--- models/instrument.py
+++ models/instrument.py
@@ -1,11 +1,12 @@
 class Instrument:
-    def __init__(self,name,ins_type,displayName,pipLocation,tradeUnitsPrecision,marginRate):
+    def __init__(self,name,ins_type,displayName,pipLocation,tradeUnitsPrecision,marginRate,displayPrecision):
         self.name = name
         self.ins_type = ins_type
         self.displayName = displayName
         self.pipLocation = pow(10,pipLocation)
         self.tradeUnitsPrecision = tradeUnitsPrecision
         self.marginRate = float(marginRate)
+        self.displayPrecision = displayPrecision
 
     def __repr__(self):
         return str(vars(self))
@@ -19,4 +20,5 @@ class Instrument:
             ob['pipLocation'],
             ob['tradeUnitsPrecision'],
             ob['marginRate'],
+            ob['displayPrecision']
         )
